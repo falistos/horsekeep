1013eab5c6626912439c91c3a6d34c5b85f12757
Pascal Ludwig <pascal.ludwig@mantro.net>
2014-05-10 23:25:05 +0200

    added craftbukkit dependency to pom.xml and updated to CB 1.7.2-R2

--- HorseKeep/src/main/java/com/gmail/falistos/HorseKeep/HorseManager.java
+++ HorseKeep/src/main/java/com/gmail/falistos/HorseKeep/HorseManager.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import net.minecraft.server.v1_6_R3.EntityInsentient;
-import net.minecraft.server.v1_6_R3.GenericAttributes;
+import net.minecraft.server.v1_7_R1.EntityInsentient;
+import net.minecraft.server.v1_7_R1.GenericAttributes;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -14,7 +14,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.craftbukkit.v1_6_R3.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftLivingEntity;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
